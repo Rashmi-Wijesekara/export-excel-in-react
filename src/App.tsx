82d6108cb34a6b3b363bdfa84cf95243505d6a9a
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2023-09-11 16:46:30 +0530

    added excel styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { Table } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import './App.css';
+const ExcelJS = require("exceljs");
 
 function App() {
+  const headerCellBorderStyle = {
+    top: { style: "thin", color: { argb: "black" } },
+    left: { style: "thin", color: { argb: "black" } },
+    bottom: { style: "thin", color: { argb: "black" } },
+    right: { style: "thin", color: { argb: "black" } },
+  };
 
   type TrainingProgressReportItemType = {
     EmployeeId: number;
@@ -184,25 +191,145 @@ function App() {
     },
   ];
 
-  const downloadHandler = () => {
+  const downloadHandler = async () => {
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet("Training Progress");
+    sheet.properties.defaultRowHeight = 20;
+    sheet.properties.columnCount = 4
+    sheet.properties.actualColumnCount = 4
 
+    // APPLY BORDER
+    sheet.getRow(5).border = {
+      top: { style: "thin", color: { argb: "black" } },
+      left: { style: "thin", color: { argb: "black" } },
+      bottom: { style: "thin", color: { argb: "black" } },
+      right: { style: "thin", color: { argb: "black" } },
+    };
+
+    sheet.getRow(5).fill = {
+      type: 'pattern',
+      pattern: 'solid',
+      fgColor: { argb: 'F0F5FF' },
+    };
+
+    /*TITLE*/
+    sheet.mergeCells('A1', 'D1');
+    sheet.getCell('A1').value = 'Training Progress Report'
+    sheet.mergeCells('A2', 'D2');
+    sheet.getCell('A2').value = `Downloaded : ${String(new Date().toLocaleString("en-US").replace(",", ""))}`
+    sheet.getCell('A1').alignment = {
+      vertical: "middle", horizontal: "center"
+    }
+    sheet.getCell('A2').alignment = {
+      vertical: "middle", horizontal: "center"
+    }
+    sheet.getRow(1).font = {
+      family: 4,
+      size: 18,
+      bold: true,
+      underline: 'double'
+    };
+    sheet.getRow(2).font = {
+      family: 4,
+      size: 16,
+      bold: true,
+    };
+    sheet.getRow(1).height = 30
+    sheet.getRow(2).height = 30
+    //  ==============================
+
+
+    sheet.getRow(5).font = {
+      family: 4,
+      size: 12,
+      bold: true,
+    };
+
+    sheet.getRow(5).values = [
+      "Employee ID", "Learner Name", "Location", "Organization"
+    ]
+
+    sheet.columns = [
+      {
+        key: "EmployeeId",
+        width: 20
+      },
+      {
+        key: "LearnerName",
+        width: 40
+      },
+      {
+        key: "Location",
+        width: 30
+      },
+      {
+        key: "Organization",
+        width: 30
+      },
+    ]
+
+    // sheet.columns = [
+    //   {
+    //     header: "ID",
+    //     key: "EmployeeId",
+    //     width: 20
+    //   },
+    //   {
+    //     header: "Learner Name",
+    //     key: "LearnerName",
+    //     width: 40
+    //   },
+    //   {
+    //     header: "Location",
+    //     key: "Location",
+    //     width: 30
+    //   },
+    //   {
+    //     header: "Organization",
+    //     key: "Organization",
+    //     width: 30
+    //   },
+    // ]
+
+    await trainingProgressDataList.map(async (item, index) => {
+      sheet.addRow({
+        EmployeeId: item?.EmployeeId,
+        LearnerName: item?.LearnerName,
+        Location: item?.Location,
+        Organization: item?.Organization
+      });
+    })
+
+    await workbook.xlsx.writeBuffer().then(function (data: any) {
+      const blob = new Blob([data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+      const url = window.URL.createObjectURL(blob);
+      const anchor = document.createElement("a");
+      anchor.href = url;
+      const timenow = String(String(new Date().toLocaleString("en-US").replace(/,|:/g, ".")))
+      anchor.download = `${timenow}.xlsx`;
+      anchor.click();
+      window.URL.revokeObjectURL(url);
+    });
   }
 
   return (
     <div className="container">
       <Button onClick={downloadHandler} className='my-4'>Download Report</Button>
       <Table striped bordered hover>
+        <caption>Training Progress Report</caption>
         <thead>
           <tr className="table-header">
             <th scope="col" className="table-heading-title">Emp. ID</th>
             <th scope="col" className="table-heading-title">Learner Name</th>
             <th scope="col" className="table-heading-title">Location</th>
             <th scope="col" className="table-heading-title">Organization</th>
-            <th scope="col" className="table-heading-title">No. Of Courses Enrolled</th>
+            {/* <th scope="col" className="table-heading-title">No. Of Courses Enrolled</th>
             <th scope="col" className="table-heading-title">Attendance Rate</th>
             <th scope="col" className="table-heading-title">Completed</th>
             <th scope="col" className="table-heading-title">In Progress</th>
-            <th scope="col" className="table-heading-title">Abandoned</th>
+            <th scope="col" className="table-heading-title">Abandoned</th> */}
 
           </tr>
         </thead>
@@ -220,11 +347,11 @@ function App() {
                   <span>{item.Location}</span>
                 </td>
                 <td id="table-data">{item.Organization}</td>
-                <td id="table-data">{item.EnrolledCourseCount}</td>
+                {/* <td id="table-data">{item.EnrolledCourseCount}</td>
                 <td id="table-data">{item.AttendanceRate}%</td>
                 <td id="table-data">{item.CompletedCourseCount}</td>
                 <td id="table-data">{item.InProgressCourseCount}</td>
-                <td id="table-data">{item.AbandonedCourseCount}</td>
+                <td id="table-data">{item.AbandonedCourseCount}</td> */}
               </tr>
             ))
           }
